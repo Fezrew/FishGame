8719bf2150dee3a17a19fd0da2a40b2cf293b44a
Dylan White <Evilwithin626@Gmail.com>
2023-02-13 16:14:42 +1030

    Minor changes(Mostly messing with visual studio settings)

---
+++ FishGame/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Contains all player input functions
+/// </summary>
+public class Player : MonoBehaviour
+{
+    public float MoveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ FishGame/Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls how the player camera looks around
+/// </summary>
+public class PlayerLook : MonoBehaviour
+{
+
+    public float MouseSensitivity = 100f;
+    private Transform playerBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //playerBody = GetComponentInParent();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+
+
+    }
+}

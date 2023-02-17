944e38d54c09671a699e3a6623e1fb5abdd56229
Dylan White <Evilwithin626@Gmail.com>
2023-02-17 15:04:35 +1030

    Added movement/gravity

--- FishGame/Assets/Scripts/Player/Player.cs
+++ FishGame/Assets/Scripts/Player/Player.cs
@@ -7,17 +7,34 @@ using UnityEngine;
 /// </summary>
 public class Player : MonoBehaviour
 {
+    CharacterController controller;
+
     public float MoveSpeed = 10f;
+    public float gravity = -9.81f;
+
+    /// <summary>
+    /// The player's y velocity used to determine the rate of falling
+    /// </summary>
+    Vector3 velocity;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * MoveSpeed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }

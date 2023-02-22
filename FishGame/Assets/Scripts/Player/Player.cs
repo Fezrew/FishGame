354d9a5a2fef85d10fc3550fcaee05ebf1b91be8
Dylan White <Evilwithin626@Gmail.com>
2023-02-22 15:01:56 +1030

    Player movement is done, now I begin fishing

--- FishGame/Assets/Scripts/Player/Player.cs
+++ FishGame/Assets/Scripts/Player/Player.cs
@@ -10,7 +10,14 @@ public class Player : MonoBehaviour
     CharacterController controller;
 
     public float MoveSpeed = 10f;
+    public float JumpForce = 3f;
     public float gravity = -9.81f;
+
+    public Transform GroundCheck;
+    public float GroundDistance = 0.4f;
+    public LayerMask GroundMask;
+
+    bool isGrounded;
 
     /// <summary>
     /// The player's y velocity used to determine the rate of falling
@@ -26,6 +33,8 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(GroundCheck.position, GroundDistance, GroundMask);
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
@@ -34,6 +43,11 @@ public class Player : MonoBehaviour
         controller.Move(move * MoveSpeed * Time.deltaTime);
 
         velocity.y += gravity * Time.deltaTime;
+
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+            velocity.y = Mathf.Sqrt(JumpForce * -2f * gravity);
 
         controller.Move(velocity * Time.deltaTime);
     }

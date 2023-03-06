954ae30359bb9b474dde8a25f56da9d77e76b241
Dylan White <dylanhwite@Gmail.com>
2023-03-06 16:36:27 +1030

    Flowchart is finished, started putting together Fishing system

---
+++ FishGame/Assets/Scripts/FezrewFishing/FishingManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FezrewFishing
+{
+    static class FishingManager
+    {
+        public static bool HasApproach = false;
+
+        public static Bite BitePhase;
+        public static Catch CatchPhase;
+    }
+}

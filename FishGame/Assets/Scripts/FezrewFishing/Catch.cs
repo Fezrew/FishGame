5c27627b213f45c04066272e6c457c0c309d1627
Dylan White <dylanhwite@Gmail.com>
2023-05-01 16:30:05 +0930

    Almost done transferring back, will be making inherited catch classes soon

--- FishGame/Assets/Scripts/FezrewFishing/Catch.cs
+++ FishGame/Assets/Scripts/FezrewFishing/Catch.cs
@@ -11,6 +11,7 @@ namespace FezrewFishing
         /// </summary>
         public static Catch instance;
 
+        #region Catch Variables
         /// <summary>
         /// What is the minigame you want to play after you've begun catch phase
         /// </summary>
@@ -34,7 +35,70 @@ namespace FezrewFishing
         /// </summary>
         [Tooltip("The minigame that begins once the player reaches catch phase")]
         public catchType CatchType;
+        /// <summary>
+        /// How close the player is to catching the fish
+        /// This should only be changed in this script
+        /// </summary>
+        private float catchProgress;
+        /// <summary>
+        /// Allows external scripts to read the catchProgress
+        /// Mostly to allow UI displays of the current progress
+        /// </summary>
+        [Tooltip("How close the player is to catching the fish (Read-only)")]
+        public float CatchProgress => catchProgress;
+        /// <summary>
+        /// The amount of progress the player must reach before the fish is caught
+        /// FOR RANGE CATCHING THIS IS THE NUMBER OF SECONDS YOU NEED TO SPEND CATCHING IT
+        /// </summary>
+        [Tooltip("The amount of progress the player must reach before the fish is caught")]
+        public float ProgressRequired;
+        #endregion
 
+        #region Range Catching Variables
+        /// <summary>
+        /// The highest value the fishRange or catchRange can get before being stopped
+        /// </summary>
+        [Tooltip("The highest value the fishRange or catchRange can get before being stopped")]
+        public float CatchBarMax;
+
+        /// <summary>
+        /// The value range of which you must keep the fishes value within in order to gain catch progress
+        /// </summary>
+        [Tooltip("The value range of which you must keep the fishes value within in order to gain catch progress")]
+        public float CatchRange;
+        /// <summary>
+        /// How fast the catch range moves in the catch bar
+        /// </summary>
+        [Tooltip("How fast the catch range moves in the catch bar")]
+        [Range(1, 10)]
+        public float RangeSpeed;
+        /// <summary>
+        /// the position of the centre of the player's catch range
+        /// </summary>
+        float rangePosition;
+        /// <summary>
+        /// The position of the centre of the fish
+        /// </summary>
+        float fishPosition;
+
+        //Read-only variables for the position values
+        public float FishPos => fishPosition;
+        public float RangePos => rangePosition;
+
+        /// <summary>
+        /// The time before the fish decides their movement direction
+        /// </summary>
+        [Tooltip("The time before the fish decides their movement direction")]
+        public float DecisionTime = 1;
+        /// <summary>
+        /// Once this equals DecisionTime, the fish decides the next direction
+        /// </summary>
+        float decisionTimer;
+        /// <summary>
+        /// Is the fish swimming upwards?
+        /// </summary>
+        bool swimUpwards;
+        #endregion
 
         public void Start()
         {
@@ -44,6 +108,12 @@ namespace FezrewFishing
                 instance = this;
             else if (instance != null && instance != this)
                 Destroy(this.gameObject);
+        }
+
+        //TODO: Create derived catch classes for each Catch Type
+        public void Update()
+        {
+            
         }
 
         /// <summary>
